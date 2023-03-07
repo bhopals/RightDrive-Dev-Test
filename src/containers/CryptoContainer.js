50a4d34436d982a01bd2434111d600aecad43c75
Bhopal Singh <bhopalsinghsis@gmail.com>
2023-03-06 19:14:52 -0500

    feat(API): use Crypto coingencko API (Refer API Docs - https://www.coingecko.com/en/api/documentation)

---
+++ src/containers/CryptoContainer.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import Currency from '../components/Currency'
+
+function CryptoContainer() {
+  const [currencies, setCurrencies] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=100&page=1'
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCurrencies(data))
+      .catch((error) => console.log('Error:', error))
+  }, [])
+
+  return (
+    <div>
+      <Currency currencies={currencies} />
+    </div>
+  )
+}
+
+export default CryptoContainer
